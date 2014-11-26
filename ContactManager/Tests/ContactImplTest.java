4b943bf4b4ac89e36e7354a86bd07ac0dd9c2860
Elmario Husha <elmario@wizardsworkshop.co.uk>
2014-11-26 23:39:55 +0000

    Added test files for Contact - Not completed yet

---
+++ ContactManager/Tests/ContactImplTest.java
@@ -0,0 +1,40 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContactImplTest {
+
+	@Test
+	public void testWeCanCreateNewContact() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanGetContactId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContactIdIsUnique() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanGetContactName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanGetContactNotes() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCanChangeContactNotes() {
+		fail("Not yet implemented");
+	}
+
+
+}
